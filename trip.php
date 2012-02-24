3e0edae734b8197bf2e6956a8aa76c192e9c77c1
Dusan Lukic <ldusan84@gmail.com>
2012-02-24 02:57:33 +0100

    Initial commit

---
+++ trip.php
@@ -0,0 +1,128 @@
+ <?php
+/**
+ * @description Trip category, creates trip according to trip cards
+ *
+ * @author Dusan Lukic <ldusan84@gmail.com>
+ */
+class Trip {
+    
+
+    var $points;
+    var $cards;
+    var $begin;
+    var $end;
+    var $moment; // current place
+	var $trip_end = true;
+    	
+	/**
+	 * Sort cards.
+	 *
+	 * @param array $cards
+	 */
+    
+    private function sort($cards){
+		
+		$this->cards = $cards;
+		
+		while(true) {
+			
+			foreach ($this->cards as $key=>$val){
+
+				// if start of the card is where we are, add card to stack
+
+				if ($val->start == $this->moment){
+
+					// add to stack
+					$this->add($val);
+
+					// set new "moment" (current place) to destination of current card
+					$this->moment = $val->destination;
+
+					// remove this card
+					unset($this->cards[$key]);
+
+				}
+			}
+			
+			// check if we reached the end of the trip
+			if($this->end == $this->moment) {
+				return;
+			}
+		}
+    }
+
+	
+	/**
+	 * Determine global start and end and prepare starting moment.
+	 *
+	 * @param array $cards
+	 */
+    
+    private function prepare($cards){
+        
+        foreach ($cards as $key=>$val){
+            $destinations[] = $val->destination;
+            $starts[] = $val->start;
+        }
+
+        foreach ($destinations as $key=>$val){
+            if (!in_array($val, $starts)){
+                $this->end = $val;
+            }
+        }
+        
+        foreach ($starts as $key=>$val){
+            if (!in_array($val, $destinations)){
+                $this->begin = $val;
+            }
+        }
+        
+        // set current place to beginning
+        
+        $this->moment = $this->begin;
+        
+    }
+	
+	/**
+	 * Add card to current points stack.
+	 *
+	 * @param Card $card
+	 */
+	
+	private function add(Card $card) {
+		
+		$this->points[] = $card;
+		
+	}
+    
+
+    
+    private function output(){
+        
+        foreach ($this->points as $key=>$val){
+            $stack[]="Take ".$val->transportation." from ".$val->start." to 
+                ".$val->destination." .The seat is: ".$val->seat;
+        }
+        $final = implode(" \n",$stack);
+        
+        return $final;
+
+    }
+	
+	
+	/**
+	 * Initialize.
+	 *
+	 * @param array $cards
+	 * 
+	 * @return string
+	 */
+	public function init($cards) {
+		
+		$this->prepare($cards);
+        $this->sort($cards);
+		return $this->output();
+	}
+}
+
+?>
